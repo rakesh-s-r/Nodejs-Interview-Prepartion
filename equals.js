198d70ce7eaa8dc89938224675589d7d8fbea79c
Rakesh SR <rakeshsr1998@gmail.com>
2026-07-25 15:05:47 +0530

    What is difference between == & === ?

---
+++ equals.js
@@ -0,0 +1,9 @@
+let a = 1;
+let b = true;
+
+console.log(a == b); // checks only value
+console.log(a === b); // check value & data type
+
+let v1 = "string";
+let v2 = 'string';
+console.log(v1 === v2)
