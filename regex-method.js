354d35b5fcf468a20d12732f0f92ecfd13c37b44
Rakesh SR <rakeshsr1998@gmail.com>
2026-07-25 20:01:42 +0530

    What is the difference between exec () and test () methods in javascript?

---
+++ regex-method.js
@@ -0,0 +1,7 @@
+// exec and test both are regex expression methods in js
+
+// exec
+let pattern = /hello/;
+console.log(pattern.exec("hello world")); // return pattern
+
+console.log(pattern.test("nothing"));
